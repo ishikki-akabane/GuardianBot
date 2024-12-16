0701d33014d761ff393882bc7101f7097158e226
Ishikki Akabane <rohansjaishwal@gmail.com>
2024-12-16 13:45:23 +0100

    Update __init__.py

--- bot/__init__.py
+++ bot/__init__.py
@@ -1,10 +1,10 @@
 
 import os
-from telehook import TeleClient
+from telehook import TeleClient, logger
 
 # Configure the bot token and webhook URL
 BOT_TOKEN = "8186222249:AAEh0MnDoUYzKvo2p1J97JabgHsoIN6v9nE"
-WEBHOOK_URL = "https://pup-solid-publicly.ngrok-free.app/webhook"
+WEBHOOK_URL = "https://pup-solid-publicly.ngrok-free.app/"
 
 # Initialize TeleHook with the plugins folder
 TeleHook = TeleClient(
@@ -12,3 +12,5 @@ TeleHook = TeleClient(
     url=WEBHOOK_URL,
     plugins={"root": "bot.plugins"}
 )
+
+LOGGER = logger
